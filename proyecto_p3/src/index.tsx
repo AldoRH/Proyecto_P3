bca1619589d09c13e6733edfdaa1facf4a14f497
Daniel López <hyperdaniel579exe@gmail.com>
2023-05-26 14:51:26 -0600

    update index

--- proyecto_p3/src/index.tsx
+++ proyecto_p3/src/index.tsx
@@ -3,19 +3,7 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import ResponsiveAppBar from "./components/ResponsiveAppBar";
 import "bootstrap/dist/css/bootstrap.css";
-import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
-import Home from "./screens/Home";
-import ProductsScreen from "./screens/ProductsScreen";
-import Product from "./screens/ProductScreen";
-import ViewProductScreen from "./screens/ViewProductScreen";
-import ServicesScreen from "./screens/ServicesScreen";
-import Service from "./screens/ServiceScreen";
-import ViewServiceScreen from "./screens/ViewServiceScreen";
-import SalesScreen from "./screens/SalesScreen";
-import SaleScreen from "./screens/SaleScreen";
-import { ViewSale } from "./screens/ViewSale";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
