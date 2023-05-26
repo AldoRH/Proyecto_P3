c0367647255fee1406538219a8f2751de8ffbf4b
Daniel López <hyperdaniel579exe@gmail.com>
2023-05-26 14:08:46 -0600

    requests for services

--- proyecto_p3/src/resources/ServicesFirebase.ts
+++ proyecto_p3/src/resources/ServicesFirebase.ts
@@ -1,41 +1,48 @@
-import { addDoc, collection, getDocs, deleteDoc, doc, updateDoc, getDoc } from "firebase/firestore";
+import {
+  addDoc,
+  collection,
+  getDocs,
+  deleteDoc,
+  doc,
+  updateDoc,
+  getDoc,
+} from "firebase/firestore";
 import { db } from "../firebase";
 
-const usersCollection = collection(db, 'services');
+const usersCollection = collection(db, "services");
 
 export const addService = async (service: any) => {
-    try {
-        await addDoc(usersCollection, service);
-        return true;
-    } catch (error) {
-        return false;
-    }
+  try {
+    await addDoc(usersCollection, service);
+    return true;
+  } catch (error) {
+    return false;
+  }
 };
 
 export const getServices = async () => {
-    const result = await getDocs(usersCollection);
-    return result;
-}; 
+  const result = await getDocs(usersCollection);
+  return result;
+};
 
 export const deleteService = async (id: any) => {
-    const docRef = doc(db, 'services', id);
-    await deleteDoc(docRef);
-    return true;
-}
+  const docRef = doc(db, "services", id);
+  await deleteDoc(docRef);
+  return true;
+};
 
 export const getServiceById = async (id: any) => {
-    const docRef = doc(db, 'services', id);
-    const result = await getDoc(docRef);
-    return result.data();
-}
+  const docRef = doc(db, "services", id);
+  const result = await getDoc(docRef);
+  return result.data();
+};
 
 export const updateService = async (id: any, service: any) => {
-    const docRef = doc(db, 'services', id);
-    try {
-        await updateDoc(docRef, service)
-        return true;
-    } catch (error) {
-        return false;
-    }
-}
-
+  const docRef = doc(db, "services", id);
+  try {
+    await updateDoc(docRef, service);
+    return true;
+  } catch (error) {
+    return false;
+  }
+};
