dd31a0007e593ce4add9d46c46110a43e8b08a32
Daniel López <hyperdaniel579exe@gmail.com>
2023-05-26 14:55:31 -0600

    path to login added

--- proyecto_p3/src/screens/SignUp.tsx
+++ proyecto_p3/src/screens/SignUp.tsx
@@ -1,35 +1,51 @@
-import React, { useState } from 'react';
-import { useNavigate } from 'react-router-dom';
-import { Box, TextField, Button } from '@mui/material';
-import { getAuth, createUserWithEmailAndPassword } from 'firebase/auth';
+import React, { useState } from "react";
+import { useNavigate, Link } from "react-router-dom";
+import { Box, TextField, Button } from "@mui/material";
+import { getAuth, createUserWithEmailAndPassword } from "firebase/auth";
 
 export const SignUp = () => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
   const auth = getAuth();
-  const navigate =useNavigate()
+  const navigate = useNavigate();
 
   const handleSignUp = async (e: React.FormEvent) => {
     e.preventDefault();
 
     try {
-      const userCredential = await createUserWithEmailAndPassword(auth, email, password);
+      const userCredential = await createUserWithEmailAndPassword(
+        auth,
+        email,
+        password
+      );
       const user = userCredential.user;
-      if(user){
-        navigate('/')
+      if (user) {
+        navigate("/");
       }
-      
     } catch (error) {
-      console.error('Error al crear el usuario:', error);
+      console.error("Error al crear el usuario:", error);
     }
   };
 
   return (
-    <div style={{ maxHeight: '100vh', boxShadow: 'initial' }}>
-      <Box display="flex" justifyContent="center" alignItems="center" height="100vh" p={2}>
-        <Box p={3} bgcolor="white" boxShadow={2} borderRadius={8} maxWidth={600} width="100%">
-          <div style={{ textAlign: 'center' }}>
+    <div style={{ maxHeight: "100vh", boxShadow: "initial" }}>
+      <Box
+        display="flex"
+        justifyContent="center"
+        alignItems="center"
+        height="100vh"
+        p={2}
+      >
+        <Box
+          p={3}
+          bgcolor="white"
+          boxShadow={2}
+          borderRadius={8}
+          maxWidth={600}
+          width="100%"
+        >
+          <div style={{ textAlign: "center" }}>
             <h1>Bienvenido!!</h1>
             <h3>Registrate</h3>
             <form onSubmit={handleSignUp}>
@@ -54,11 +70,30 @@ export const SignUp = () => {
                   onChange={(e) => setPassword(e.target.value)}
                 />
               </Box>
-              <Box width={'100%'} display="flex" justifyContent="center" alignItems="center">
+              <Box
+                width={"100%"}
+                display="flex"
+                justifyContent="center"
+                alignItems="center"
+              >
                 <div>
-                  <Button color="primary" size="large" variant="contained" type="submit">
+                  <Button
+                    color="primary"
+                    size="large"
+                    variant="contained"
+                    type="submit"
+                  >
                     Registrate
                   </Button>
+                  <p style={{ color: "#6F7B8C", paddingTop: "30px" }}>
+                    Ya tienes una cuenta?{" "}
+                    <Link
+                      to={"/"}
+                      style={{ font: "caption", color: "#3B54AD" }}
+                    >
+                      Inicia Sesión
+                    </Link>
+                  </p>
                 </div>
               </Box>
             </form>
