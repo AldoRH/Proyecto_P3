2eb0dd8e48fbb6dd350955aeb2b7c869b28e1bc5
Daniel López <hyperdaniel579exe@gmail.com>
2023-05-29 21:46:24 -0600

    alert updated

--- proyecto_p3/src/components/Alert.tsx
+++ proyecto_p3/src/components/Alert.tsx
@@ -1,14 +1,32 @@
-import React from 'react';
-import { Stack } from '@mui/material';
-import { Alert, AlertTitle } from '@mui/material';
+import React from "react";
+import { Stack } from "@mui/material";
+import { Alert, AlertTitle } from "@mui/material";
 
-export const AlertsError: React.FC<{title: string, message: string}> = ({ title, message}) =>{    
-    return(        
-        <Stack sx={{ width: '100%' }} spacing={2}>
-            <Alert severity='error'>
-                <AlertTitle>{title}</AlertTitle>
-               {message}
-            </Alert>
-        </Stack>        
-    )
-}
+export const AlertsError: React.FC<{ title: string; message: string }> = ({
+  title,
+  message,
+}) => {
+  return (
+    <Stack sx={{ width: "100%" }} spacing={2}>
+      <Alert severity="error">
+        <AlertTitle>{title}</AlertTitle>
+        {message}
+      </Alert>
+    </Stack>
+  );
+};
+
+export const AlertInformation: React.FC<{ title: string; message: string }> = ({
+  title,
+  message,
+}) => {
+  return (
+    <Stack sx={{ width: "100%" }} spacing={2}>
+      <Alert severity="warning">
+        <AlertTitle>{title}</AlertTitle>
+
+        {message}
+      </Alert>
+    </Stack>
+  );
+};
